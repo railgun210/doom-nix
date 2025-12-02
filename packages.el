29312088dbd1595bfb6d33a59fff0281c9d381c9
Railgun <aday56709@gmail.com>
2025-12-01 21:19:18 -0600

    Pinned everforest package to specific commit

--- packages.el
+++ packages.el
@@ -15,7 +15,7 @@
 ;;   :recipe (:host github :repo "username/repo"))
 
 ;; Everforest Theme
-(package! everforest :recipe (:repo "https://git.sr.ht/~theorytoe/everforest-theme"))
+(package! everforest :recipe (:repo "https://git.sr.ht/~theorytoe/everforest-theme") :pin "703b16b742b753f6ad077b5c7f51947d1926c530")
 
 ;; If the package you are trying to install does not contain a PACKAGENAME.el
 ;; file, or is located in a subdirectory of the repo, you'll need to specify
